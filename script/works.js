601f1181f151006b60991fc1e91dbb03bafc96c1
lghayy <lghayy@foxmail.com>
2017-12-31 00:01:23 +0800

    ..

--- script/works.js
+++
@@ -1,9 +0,0 @@
-import imagesLoaded from 'imagesloaded'; //防止获取不到图片的高度
-const Masonry = require("./lib/masonry.pkgd.min"),
-	jQueryBridget = require("./lib/jquery-bridget");
-jQueryBridget("masonry", Masonry);
-$("#worksBox").imagesLoaded(() => {
-	$("#worksBox").masonry({
-		itemSelector: ".grid-item"
-	});
-})
