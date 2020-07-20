600471ef285abaaa2d3b5a4b60a98ed05317882d
lgh <lghayy@foxmail.com>
2020-07-20 23:59:16 +0800

    修改配置

--- config/index.js
+++ config/index.js
@@ -2,8 +2,8 @@
   self.g_config = factory()
 })(global || window || {}, function () {
   const isProd = process.env.NODE_ENV === 'production'
-  // const visitPath = 'https://liuguanhua.github.io/'
-  const visitPath = 'http://localhost:5000/'
+  const visitPath = 'https://liuguanhua.github.io/'
+  // const visitPath = 'http://localhost:5000/'
   const publicPath = isProd ? visitPath : '/'
   return {
     name: '刘关化|个人简历|前端工程师',
